9284161a179c07cd262744083f6e78f39abe8d50
Amer <amer.hasanbegovic@edu.fit.ba>
2021-11-12 04:44:26 +0100

    changed register page layout

---
+++ DevForum/DevForum/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using DevForum.Data;
+using DevForum.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DevForum.Areas.Identity.IdentityHostingStartup))]
+namespace DevForum.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
